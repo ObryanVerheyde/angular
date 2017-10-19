c3f9c00551f063da5d280abc9840fa06c30ffbe9
O'Bryan Verheyde <verheyde.obryan@gmail.com>
2017-10-19 11:25:28 +0200

    add task todo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Task } from './task.model';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  tasks = [];
+  taskText = '';
+  tasks: Task[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTask(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      this.tasks.push(new Task(this.taskText, false));
+      this.taskText = '';
+    }
+  }
 }
