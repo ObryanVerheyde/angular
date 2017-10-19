48719b8b26f9515a4315db7c0a03c47ed9f06718
O'Bryan Verheyde <verheyde.obryan@gmail.com>
2017-10-19 10:17:28 +0200

    change color on maxLength twitter component

--- src/app/twitter/twitter.component.ts
+++ src/app/twitter/twitter.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-twitter',
-  templateUrl: './twitter.component.html'
+  templateUrl: './twitter.component.html',
+  // styleUrls: ['./twitter.component.css'],
+  styles: [`
+    .textColor {
+      color: blue
+    }
+  `]
 })
 
 export class TwitterComponent {
+  maxLength = 140;
   monTextArea = 'Bonjour';
   txt = "";
   changeColor = ""
